60248d3007500656da5525a1e81bd9142f09d170
edgib102 <58235933+edgib102@users.noreply.github.com>
2020-08-28 21:29:42 +1200

    Add files via upload

---
+++ Python_Challenges/Q4_MaxEdgeTriangle.py
@@ -0,0 +1,14 @@
+print("finds the maximum range of a triangle's third edge")
+print("Type side one length")
+num1 = int(input())
+print("Type side two length")
+num2 = int(input())
+
+def next_edge(side1, side2):
+    answr = (side1 + side2)-1
+    print(answr)
+    return answr
+
+next_edge(num1, num2)
+
+# challenge reference: https://edabit.com/challenge/Zerwo2AENbvRZTe83
