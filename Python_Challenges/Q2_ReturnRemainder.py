2cae7be61bc7bcb40e87e9533a11fb1f17cd6f92
edgib102 <58235933+edgib102@users.noreply.github.com>
2020-08-28 21:04:47 +1200

    Add files via upload

---
+++ Python_Challenges/Q2_ReturnRemainder.py
@@ -0,0 +1,15 @@
+print("Type number one")
+num1 = int(input())
+print("Type number two")
+num2 = int(input())
+
+
+def remainder(x, y):
+    remainderAwns = x % y
+    print("your answer is:")
+    print(remainderAwns)
+    return remainderAwns
+
+remainder(num1, num2)
+
+#challenge refrence: https://edabit.com/challenge/KWoj7kWiHRqJtG6S2
