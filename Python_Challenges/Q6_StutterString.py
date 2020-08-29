12f8fe640a020003cfddf86f394194c7d438d5e7
edgib102 <58235933+edgib102@users.noreply.github.com>
2020-08-29 17:05:15 +1200

    Create Q6_StutterString.py

---
+++ Python_Challenges/Q6_StutterString.py
@@ -0,0 +1,16 @@
+print("type word to stutter:")
+def stutter(word):
+    cutword = word[0:2]
+    print(cutword)
+    result = cutword + "... " + cutword + "... " + word + "?"
+    print(result)
+    return(result)
+
+
+stutter(input())
+    
+
+
+
+#Goal is to make a string to stutter like this: "in... in... incredible?" (any word)
+#Challenge from https://edabit.com/challenge/gt9LLufDCMHKMioh2
