c57f590561b89a9575e1894b11fd53432fd405c8
edgib102 <58235933+edgib102@users.noreply.github.com>
2020-08-30 19:16:11 +1200

    created eflif.py and some folders

---
+++ Progression/Loops/elif.py
@@ -0,0 +1,16 @@
+i = 10
+q = 10
+while True:
+    
+    if i != q:
+        print (i)
+        i += q 
+    elif i == q:
+        print (q)
+        i -= q
+    
+#infinitly alternates between 0 and 10
+        
+
+    
+
