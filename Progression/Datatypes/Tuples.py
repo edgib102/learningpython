1f813fb0471bcf75ba48a6f75e43393aa97d61a7
edgib102 <58235933+edgib102@users.noreply.github.com>
2020-09-01 00:28:39 +1200

    Create Tuples.py

---
+++ Progression/Datatypes/Tuples.py
@@ -0,0 +1,6 @@
+fruit = ("apple", "banana", "cherry", "orange", "strawberry")
+
+for x in fruit:
+    print(x)
+#Tuples cant be changes unless converted into a string
+#BEcause tuples cant be changed they are faster than a string
