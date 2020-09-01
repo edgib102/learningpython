90a284b61cf4baaf20b402c3b7861a2d68e51b07
edgib102 <58235933+edgib102@users.noreply.github.com>
2020-09-02 02:18:49 +1200

    Create Dictionaries.py

---
+++ Progression/Datatypes/Dictionaries.py
@@ -0,0 +1,12 @@
+a_dict = {
+    "name": "John",
+    "age": 48,
+    "hobbies": "drinking",
+    "state": "enraged & depressed due to marital disputes"
+}
+
+if "drinking" in a_dict.values():
+    a_dict["smoking"] = "suspected to be smoking, finacial situation needs to be observed"
+
+for x, y in a_dict.items():
+    print(x + ": " + str(y) + "\n")
