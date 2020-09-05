ac0f6cf58f0d3c1466a53a21e8063f89918c582b
edgib102 <58235933+edgib102@users.noreply.github.com>
2020-09-06 02:14:36 +1200

    Made reddit comment grabber

---
+++ Misc/Projects/reddit/AskRedditText/RedditCommentGrabber.py
@@ -0,0 +1,27 @@
+import praw
+
+username = 'edgib102'
+password = 'Edgib101'
+path = "/Users/bridgibson/Documents/GitHub/learningpython/Misc/Projects/reddit/AskRedditText/txt_files/test.txt"
+afile = open(path, 'w')
+afile.truncate
+
+reddit = praw.Reddit(client_id = 'syWXRTZJoN3LHw' , client_secret = 'fk1c1MVSD1XKW1wr3w7wYqpjBgg', username = username, password = password, user_agent = "prawtest")
+
+subreddit = reddit.subreddit('askreddit')
+subreddit_hot = subreddit.hot(limit=1)
+
+for submission in subreddit_hot:
+
+    #print(dir(submission))
+    submission.save()
+    comments = submission.comments
+
+    for comment in comments:
+        print('\n', 20*'-', '\n')
+        try:
+            afile.write(comment.body)
+            afile.write('\n\n')
+        except:
+            break
+print("ended")
